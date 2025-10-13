2081ed73f918e679c946876d3964059a46fd360f
benvansteenbergen <ben.vansteenbergen@itonomy.nl>
2025-10-13 23:33:29 +0200

    Adding / Removing etc

--- lib/branding.ts
+++ lib/branding.ts
@@ -13,7 +13,7 @@ export const BRAND: Record<string, Branding> = {
         domain: "emotion.wingsuite.io",
         name: "Emotion Digital",
         logo: "/emotion/logo.svg",
-        loginImage: "/wingsuite/hero.svg",
+        loginImage: "/emotion/hero.svg",
         loginBg: "#32a852",
         primaryColor: "#2c3e50",
     },
